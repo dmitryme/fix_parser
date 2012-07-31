0952a9c8c43acae75b1f2b751319ddd74998dac3
Dmitry Melnikov <dmitryme@gmail.com>
2012-07-31 16:22:40 +0400

    Update include/fix_tag.h

--- include/fix_tag.h
+++ include/fix_tag.h
@@ -2,22 +2,21 @@
 /// @author Dmitry S. Melnikov, dmitryme@gmail.com
 /// @date   Created on: 07/25/2012 03:35:40 PM
 
+#ifndef FIX_PARSER_FIX_TAG_H
+#define FIX_PARSER_FIX_TAG_H
+
+#include "fix_types.h"
+
 #include <stdint.h>
 
 #define TABLE_SIZE 64
-
-enum FIXTagType
-{
-   FIXTagValue = 1,
-   FIXTagGroup = 2
-};
 
 typedef struct FIXTagTable_ FIXTagTable;
 
 typedef struct FIXTag_
 {
    uint32_t num;
-   enum FIXTagType type;
+   FIXTagType type;
    union
    {
       struct
@@ -34,13 +33,15 @@ typedef struct FIXTag_
    struct FIXTag_* next;
 } FIXTag;
 
-FIXTagTable* new_table();
-void free_table(FIXTagTable* tbl);
+FIXTagTable* new_fix_table();
+void free_fix_table(FIXTagTable* tbl);
 
-FIXTag* set_fix_tag(FIXTagTable* tbl, uint32_t tagNum, unsigned char const* data, uint32_t len);
-FIXTag* get_fix_tag(FIXTagTable* tbl, uint32_t tagNum);
-int rm_fix_tag(FIXTagTable* tbl, uint32_t tagNum);
+FIXTag* set_fix_table_tag(FIXTagTable* tbl, uint32_t tagNum, unsigned char const* data, uint32_t len);
+FIXTag* get_fix_table_tag(FIXTagTable* tbl, uint32_t tagNum);
+int del_fix_table_tag(FIXTagTable* tbl, uint32_t tagNum);
 
-FIXTagTable* add_fix_group(FIXTagTable* tbl, uint32_t tagNum);
-FIXTagTable* get_fix_group(FIXTagTable* tbl, uint32_t tagNum, uint32_t grpIdx);
-int rm_fix_group(FIXTagTable* tbl, uint32_t tagNum, uint32_t grpIdx);
+FIXTagTable* add_fix_table_group(FIXTagTable* tbl, uint32_t tagNum);
+FIXTagTable* get_fix_table_group(FIXTagTable* tbl, uint32_t tagNum, uint32_t grpIdx);
+int del_fix_table_group(FIXTagTable* tbl, uint32_t tagNum, uint32_t grpIdx);
+
+#endif // FIX_PARSER_FIX_TAG_H
