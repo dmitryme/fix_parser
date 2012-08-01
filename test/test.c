01b689a5227ca54ed971d49258109bcbb84facf7
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-01 17:54:06 +0400

    Update test/test.c

--- test/test.c
+++ test/test.c
@@ -9,19 +9,21 @@ int main()
    int res = fix_protocol_init("fix44.xml");
    if (res == FIX_FAILED)
    {
-      printf("ERROR: %s\n", get_fix_last_error()->text);
+      printf("ERROR: %s\n", get_fix_error_text());
+      return 1;
    }
 
    struct timespec start, stop;
 
    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 
-   for(int i = 0; i < 10000; ++i)
+   for(int i = 0; i < 100000; ++i)
    {
-      FIXMessage* msg = create_fix_message(FIX44, "D", 0);
+      FIXMessage* msg = new_fix_message(FIX44, "8", 0, 0);
       if (!msg)
       {
-        printf("ERROR: %s\n", get_fix_last_error()->text);
+         printf("ERROR: %s\n", get_fix_error_text());
+         return 1;
       }
 
       set_tag_string(msg, NULL, 49, "QWERTY_12345678");
@@ -29,7 +31,7 @@ int main()
       set_tag_long(msg, NULL, 34, 34);
       set_tag_string(msg, NULL, 57, "srv-ivanov_ii1");
       set_tag_string(msg, NULL, 52, "20120716-06:00:16.230");
-      set_tag_long(msg, NULL, 37, 1);
+      set_tag_string(msg, NULL, 37, "1");
       set_tag_string(msg, NULL, 11, "CL_ORD_ID_1234567");
       set_tag_string(msg, NULL, 17, "FE_1_9494_1");
       set_tag_char(msg, NULL, 150, '0');
@@ -37,16 +39,17 @@ int main()
       set_tag_string(msg, NULL, 1, "ZUM");
       set_tag_string(msg, NULL, 55, "RTS-12.12");
       set_tag_char(msg, NULL, 54, '1');
-      set_tag_long(msg, NULL, 38, 25);
+      set_tag_float(msg, NULL, 38, 25);
       set_tag_float(msg, NULL, 44, 135155.0);
       set_tag_char(msg, NULL, 59, '0');
-      set_tag_long(msg, NULL, 32, 0);
+      set_tag_float(msg, NULL, 32, 0);
       set_tag_float(msg, NULL, 31, 0.0);
-      set_tag_long(msg, NULL, 151, 25);
-      set_tag_long(msg, NULL, 14, 0);
+      set_tag_float(msg, NULL, 151, 25.0);
+      set_tag_float(msg, NULL, 14, 0);
       set_tag_float(msg, NULL, 6, 0.0);
       set_tag_char(msg, NULL, 21, '1');
       set_tag_string(msg, NULL, 58, "COMMENT12");
+      free_fix_message(msg);
    }
 
    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
