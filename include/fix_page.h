ebb7b013abc02547cf69f770f6dab485ebb0e32d
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-02 18:13:39 +0400

    Update include/fix_page.h

---
+++ include/fix_page.h
@@ -0,0 +1,16 @@
+/// @file   fix_page.h
+/// @author Dmitry S. Melnikov, dmitryme@gmail.com
+/// @date   Created on: 08/02/2012 04:34:14 PM
+
+#ifndef FIX_PARSER_FIX_PAGE_H
+#define FIX_PARSER_FIX_PAGE_H
+
+typedef struct FIXPage_
+{
+   uint32_t size;
+   uint32_t offset;
+   struct FIXPage_* next;
+   char data[1];
+} __attribute__((packed)) FIXPage;
+
+#endif // FIX_PARSER_FIX_PAGE_H
