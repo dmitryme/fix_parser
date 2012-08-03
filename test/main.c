7705c4e5b7c9dc9b2ff419c0692f13b539e0c17a
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-03 14:40:44 +0400

    Update test/main.c

---
+++ test/main.c
@@ -0,0 +1,14 @@
+#include <check.h>
+#include <stdlib.h>
+
+extern Suite* make_fix_tag_tests_suite();
+
+int main()
+{
+   Suite* s = make_fix_tag_tests_suite();
+   SRunner* sr = srunner_create(s);
+   srunner_run_all(sr, CK_NORMAL);
+   int number_failed = srunner_ntests_failed(sr);
+   srunner_free(sr);
+   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
