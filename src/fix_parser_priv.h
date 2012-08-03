eb8c8fcf751487f0e04e634e8d4c95c873e81664
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-03 11:31:10 +0400

    Update src/fix_parser_priv.h

---
+++ src/fix_parser_priv.h
@@ -0,0 +1,37 @@
+/// @file   fix_parser_priv.h
+/// @author Dmitry S. Melnikov, dmitryme@gmail.com
+/// @date   Created on: 07/30/2012 10:54:30 AM
+
+#ifndef FIX_PARSER_FIX_PARSER_PRIV_H
+#define FIX_PARSER_FIX_PARSER_PRIV_H
+
+#include "fix_types.h"
+#include "fix_protocol_descr.h"
+#include "fix_page.h"
+
+#include <stdint.h>
+#include <stdarg.h>
+
+#define SOH 0x01
+#define ERROR_TXT_SIZE 1024
+
+struct FIXParser_
+{
+   FIXProtocolDescr* protocols[FIX_MUST_BE_LAST_DO_NOT_USE_OR_CHANGE_IT - 1];
+   int err_code;
+   char err_text[ERROR_TXT_SIZE];
+   uint32_t flags;
+   FIXPage* page;
+   FIXPage* free_page;
+   uint32_t num_pages;
+   uint32_t page_size;
+};
+
+FIXPage* fix_parser_get_page(FIXParser* parser);
+void fix_parser_free_page(FIXParser* parser, FIXPage* page);
+void set_fix_va_error(FIXParser* parser, int code, char const* text, va_list ap);
+void set_fix_error(FIXParser* parser, int code, char const* text, ...);
+void reset_fix_error(FIXParser* parser);
+FIXProtocolDescr* get_fix_protocol_descr(FIXParser* parser, FIXProtocolVerEnum ver);
+
+#endif // FIX_PARSER_FIX_PARSER_PRIV_H
