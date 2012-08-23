2945493e19aa500a6498eb10f7e8473c30254606
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-23 16:53:26 +0400

    Update src/fix_message_priv.h

--- src/fix_message_priv.h
+++ src/fix_message_priv.h
@@ -19,11 +19,14 @@ struct FIXMessage_
    FIXGroup* tags;
    FIXPage* pages;
    FIXPage* curr_page;
+   FIXGroup* used_groups;
 };
 
 void* fix_message_alloc(FIXMessage* msg, uint32_t size);
 void* fix_message_realloc(FIXMessage* msg, void* ptr, uint32_t size);
 FIXTag* fix_message_set_tag(FIXMessage* msg, FIXGroup* grp, uint32_t tagNum, unsigned char const* data, uint32_t len);
 FIXTag* fix_message_set_tag_fmt(FIXMessage* msg, FIXGroup* grp, uint32_t tagNum, char const* fmt, ...);
+FIXGroup* fix_message_get_group(FIXMessage* msg);
+void fix_message_free_group(FIXMessage* msg, FIXGroup* grp);
 
 #endif /* FIX_PARSER_FIX_MESSAGE_PRIV_H */
