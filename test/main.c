57c8148e510b06ad3aae77a2b859ba3d464ad5a2
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-23 13:09:47 +0400

    Update test/main.c

--- test/main.c
+++ test/main.c
@@ -1,18 +1,19 @@
-/// @file   main.c
-/// @author Dmitry S. Melnikov, dmitryme@gmail.com
-/// @date   Created on: 08/03/2012 02:41:31 PM
+/* @file   main.c
+   @author Dmitry S. Melnikov, dmitryme@gmail.com
+   @date   Created on: 08/03/2012 02:41:31 PM
+*/
 
 #include <check.h>
 #include <stdlib.h>
 
 extern Suite* make_fix_tag_tests_suite();
+extern Suite* make_fix_parser_tests_suite();
 
 int main()
 {
-   Suite* s = make_fix_tag_tests_suite();
-   SRunner* sr = srunner_create(s);
+   SRunner* sr = srunner_create(make_fix_parser_tests_suite());
+   srunner_add_suite(sr, make_fix_tag_tests_suite());
    srunner_run_all(sr, CK_NORMAL);
    int number_failed = srunner_ntests_failed(sr);
-   srunner_free(sr);
    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
