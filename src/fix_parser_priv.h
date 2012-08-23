a2e060d6cbdecbb52196e843122e3593d9936768
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-23 13:01:50 +0400

    Update src/fix_parser_priv.h

--- src/fix_parser_priv.h
+++ src/fix_parser_priv.h
@@ -19,18 +19,17 @@
 
 struct FIXParser_
 {
-   FIXProtocolDescr* protocols[FIX_MUST_BE_LAST_DO_NOT_USE_OR_CHANGE_IT - 1];
+   FIXProtocolDescr* protocols[FIX_MUST_BE_LAST_DO_NOT_USE_OR_CHANGE_IT];
    int err_code;
    char err_text[ERROR_TXT_SIZE];
-   uint32_t flags;
-   FIXPage* pages;
-   FIXPage* free_page;
-   uint32_t num_pages;
+   FIXParserFlags flags;
+   FIXPage* page;
+   uint32_t used_pages;
    uint32_t max_pages;
    uint32_t page_size;
-   FIXGroup* groups;
-   FIXGroup* free_group;
-   uint32_t num_groups;
+   uint32_t max_page_size;
+   FIXGroup* group;
+   uint32_t used_groups;
    uint32_t max_groups;
 };
 
@@ -38,9 +37,9 @@ FIXPage* fix_parser_get_page(FIXParser* parser, uint32_t pageSize);
 void fix_parser_free_page(FIXParser* parser, FIXPage* page);
 FIXGroup* fix_parser_get_group(FIXParser* parser);
 void fix_parser_free_group(FIXParser* parser, FIXGroup* group);
-void set_fix_va_error(FIXParser* parser, int code, char const* text, va_list ap);
-void set_fix_error(FIXParser* parser, int code, char const* text, ...);
-void reset_fix_error(FIXParser* parser);
-FIXProtocolDescr* get_fix_protocol_descr(FIXParser* parser, FIXProtocolVerEnum ver);
+void fix_parser_set_va_error(FIXParser* parser, int code, char const* text, va_list ap);
+void fix_parser_set_error(FIXParser* parser, int code, char const* text, ...);
+void fix_parser_reset_error(FIXParser* parser);
+FIXProtocolDescr* fix_parser_get_pdescr(FIXParser* parser, FIXProtocolVerEnum ver);
 
 #endif /* FIX_PARSER_FIX_PARSER_PRIV_H */
