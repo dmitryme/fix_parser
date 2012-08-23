e72a06928c3ce74cad404cb110be0795ab36341a
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-23 12:58:59 +0400

    Update perf_test/perf_test.c

--- perf_test/perf_test.c
+++ perf_test/perf_test.c
@@ -5,7 +5,8 @@
 
 int main()
 {
-   FIXParser* parser = new_fix_parser(512, 1000, 0, 1000, 0, FIX_PARSER_FLAGS_VALIDATE);
+   FIXParser* parser = new_fix_parser(512, 0, 2, 0, 1000, 0, 0/*FIXParserFlags_Validate*/);
+
    int res = fix_protocol_init(parser, "fix44.xml");
    if (res == FIX_FAILED)
    {
@@ -26,29 +27,29 @@ int main()
          return 1;
       }
 
-      set_tag_string(msg, NULL, 49, "QWERTY_12345678");
-      set_tag_string(msg, NULL, 56, "ABCQWE_XYZ");
-      set_tag_long(msg, NULL, 34, 34);
-      set_tag_string(msg, NULL, 57, "srv-ivanov_ii1");
-      set_tag_string(msg, NULL, 52, "20120716-06:00:16.230");
-      set_tag_string(msg, NULL, 37, "1");
-      set_tag_string(msg, NULL, 11, "CL_ORD_ID_1234567");
-      set_tag_string(msg, NULL, 17, "FE_1_9494_1");
-      set_tag_char(msg, NULL, 150, '0');
-      set_tag_char(msg, NULL, 39, '1');
-      set_tag_string(msg, NULL, 1, "ZUM");
-      set_tag_string(msg, NULL, 55, "RTS-12.12");
-      set_tag_char(msg, NULL, 54, '1');
-      set_tag_float(msg, NULL, 38, 25);
-      set_tag_float(msg, NULL, 44, 135155.0);
-      set_tag_char(msg, NULL, 59, '0');
-      set_tag_float(msg, NULL, 32, 0);
-      set_tag_float(msg, NULL, 31, 0.0);
-      set_tag_float(msg, NULL, 151, 25.0);
-      set_tag_float(msg, NULL, 14, 0);
-      set_tag_float(msg, NULL, 6, 0.0);
-      set_tag_char(msg, NULL, 21, '1');
-      set_tag_string(msg, NULL, 58, "COMMENT12");
+      set_fix_tag_string(msg, NULL, 49, "QWERTY_12345678");
+      set_fix_tag_string(msg, NULL, 56, "ABCQWE_XYZ");
+      set_fix_tag_long(msg, NULL, 34, 34);
+      set_fix_tag_string(msg, NULL, 57, "srv-ivanov_ii1");
+      set_fix_tag_string(msg, NULL, 52, "20120716-06:00:16.230");
+      set_fix_tag_string(msg, NULL, 37, "1");
+      set_fix_tag_string(msg, NULL, 11, "CL_ORD_ID_1234567");
+      set_fix_tag_string(msg, NULL, 17, "FE_1_9494_1");
+      set_fix_tag_char(msg, NULL, 150, '0');
+      set_fix_tag_char(msg, NULL, 39, '1');
+      set_fix_tag_string(msg, NULL, 1, "ZUM");
+      set_fix_tag_string(msg, NULL, 55, "RTS-12.12");
+      set_fix_tag_char(msg, NULL, 54, '1');
+      set_fix_tag_float(msg, NULL, 38, 25);
+      set_fix_tag_float(msg, NULL, 44, 135155.0);
+      set_fix_tag_char(msg, NULL, 59, '0');
+      set_fix_tag_float(msg, NULL, 32, 0);
+      set_fix_tag_float(msg, NULL, 31, 0.0);
+      set_fix_tag_float(msg, NULL, 151, 25.0);
+      set_fix_tag_float(msg, NULL, 14, 0);
+      set_fix_tag_float(msg, NULL, 6, 0.0);
+      set_fix_tag_char(msg, NULL, 21, '1');
+      set_fix_tag_string(msg, NULL, 58, "COMMENT12");
       free_fix_message(msg);
    }
 
