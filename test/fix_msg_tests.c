e9af60703685cb96d6d4294ca35961f1ab42bf56
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-28 13:54:58 +0400

    Update test/fix_msg_tests.c

--- test/fix_msg_tests.c
+++ test/fix_msg_tests.c
@@ -5,6 +5,8 @@
 
 #include <fix_msg.h>
 #include <fix_parser.h>
+#include <fix_msg_priv.h>
+#include <fix_parser_priv.h>
 
 #include <check.h>
 
@@ -12,6 +14,23 @@ START_TEST(CreateMsgTest)
 {
    FIXParser* p = fix_parser_create(512, 0, 2, 0, 2, 0, 0);
    fail_unless(p != NULL);
+   FIXGroup* group = p->group;
+   fail_unless(fix_protocol_init(p, "fix44.xml") == FIX_SUCCESS);
+
+   FIXMsg* msg = fix_msg_create(p, FIX44, "D");
+   fail_unless(msg != NULL);
+   fail_unless(msg->used_groups == group);
+
+   char buff[10];
+   fail_unless(fix_msg_get_string(msg, NULL, 8, buff, sizeof(buff)) == 7);
+   fail_unless(!strcmp(buff, "FIX.4.4"));
+   char msgType[10];
+   fail_unless(fix_msg_get_string(msg, NULL, 35, msgType, sizeof(msgType)) == 1);
+   fail_unless(!strcmp(msgType, "D"));
+
+   fail_unless(fix_msg_set_string(msg, NULL, 11, "ClOrdID") == FIX_SUCCESS);
+
+   fix_msg_free(msg);
    fix_parser_free(p);
 }
 END_TEST
