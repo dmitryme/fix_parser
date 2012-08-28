cabd8ae8342dad9d130133efd679675f3b1a5159
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-28 13:50:08 +0400

    Update include/fix_msg.h

--- include/fix_msg.h
+++ include/fix_msg.h
@@ -1,38 +1,113 @@
-/* @file   fix_msg.h
+/* @file   fix_types.h
    @author Dmitry S. Melnikov, dmitryme@gmail.com
-   @date   Created on: 07/30/2012 06:28:42 PM
+   @date   Created on: 07/31/2012 10:22:19 AM
 */
 
-#ifndef FIX_PARSER_FIX_MSG_H
-#define FIX_PARSER_FIX_MSG_H
+#ifndef FIX_PARSER_FIX_TYPES_H
+#define FIX_PARSER_FIX_TYPES_H
 
-#include "fix_types.h"
+#define FIX_FAILED                        -1
+#define FIX_SUCCESS                        0
+#define FIX_ERROR_TAG_HAS_WRONG_TYPE       1
+#define FIX_ERROR_TAG_NOT_FOUND            2
+#define FIX_ERROR_GROUP_WRONG_INDEX        3
+#define FIX_ERROR_XML_ATTR_NOT_FOUND       4
+#define FIX_ERROR_XML_ATTR_WRONG_VALUE     5
+#define FIX_ERROR_PROTOCOL_XML_LOAD_FAILED 6
+#define FIX_ERROR_UNKNOWN_FIELD            7
+#define FIX_ERROR_WRONG_PROTOCOL_VER       8
+#define FIX_ERROR_DUPLICATE_FIELD_DESCR    9
+#define FIX_ERROR_UNKNOWN_MSG             10
+#define FIX_ERROR_LIBXML                  11
+#define FIX_ERROR_INVALID_ARGUMENT        12
+#define FIX_ERROR_MALLOC                  13
+#define FIX_ERROR_UNKNOWN_PROTOCOL_DESCR  14
+#define FIX_ERROR_NO_MORE_PAGES           15
+#define FIX_ERROR_NO_MORE_GROUPS          16
+#define FIX_ERROR_TOO_BIG_PAGE            17
 
-#include <stdint.h>
+typedef struct FIXTagTable_ FIXGroup;
+typedef struct FIXTag_ FIXTag;
+typedef struct FIXMsg_ FIXMsg;
+typedef struct FIXParser_ FIXParser;
 
-FIXMsg* fix_msg_create(FIXParser* parser, FIXProtocolVerEnum ver, char const* msgType);
-void fix_msg_free(FIXMsg* msg);
+typedef enum FixParserFlags_
+{
+   FixParserFlags_CheckCRC = 0x01,
+   FIXParserFlags_Validate = 0x02
+} FIXParserFlags;
 
-FIXGroup* fix_msg_add_group(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
-FIXGroup* fix_msg_get_group(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, uint32_t grpIdx);
-int fix_msg_del_group(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, uint32_t grpIdx);
+typedef enum FIXTagType_
+{
+   FIXTagType_Value = 1,
+   FIXTagType_Group = 2
+} FIXTagType;
 
-int fix_msg_set_string(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, char const* val);
-int fix_msg_set_int(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, int32_t val);
-int fix_msg_set_uint(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, uint32_t val);
-int fix_msg_set_int64(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, int64_t val);
-int fix_msg_set_uint64(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, uint64_t val);
-int fix_msg_set_char(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, char val);
-int fix_msg_set_float(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, float val);
+typedef enum FIXProtocolVerEnum_
+{
+   FIX42,
+   FIX44,
+   FIX50,
+   FIX50SP1,
+   FIX50SP2,
+   FIXT11,
+   FIX_MUST_BE_LAST_DO_NOT_USE_OR_CHANGE_IT
+} FIXProtocolVerEnum;
 
-int fix_msg_get_int(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, int32_t* val);
-int fix_msg_get_uint(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, uint32_t* val);
-int fix_msg_get_int64(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, int64_t* val);
-int fix_msg_get_uint64(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, uint64_t* val);
-int fix_msg_get_float(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, float* val);
-int fix_msg_get_char(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, char* val);
-int fix_msg_get_string(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, char* val, uint32_t len);
+typedef enum FIXFieldTypeEnum_
+{
+   FIXFieldType_Unknown              = -1,
+   FIXFieldType_Int                  = 1,
+   FIXFieldType_Length               = 2,
+   FIXFieldType_NumInGroup           = 3,
+   FIXFieldType_SeqNum               = 4,
+   FIXFieldType_TagNum               = 5,
+   FIXFieldType_DayOfMonth           = 6,
+   FIXFieldType_Float                = 7,
+   FIXFieldType_Qty                  = 8,
+   FIXFieldType_Price                = 9,
+   FIXFieldType_PriceOffset          = 10,
+   FIXFieldType_Amt                  = 11,
+   FIXFieldType_Percentage           = 12,
+   FIXFieldType_Char                 = 13,
+   FIXFieldType_Boolean              = 14,
+   FIXFieldType_String               = 15,
+   FIXFieldType_MultipleValueString  = 16,
+   FIXFieldType_Country              = 17,
+   FIXFieldType_Currency             = 18,
+   FIXFieldType_Exchange             = 19,
+   FIXFieldType_MonthYear            = 20,
+   FIXFieldType_UTCTimestamp         = 21,
+   FIXFieldType_UTCTimeOnly          = 22,
+   FIXFieldType_UTCDateOnly          = 23,
+   FIXFieldType_LocalMktDate         = 24,
+   FIXFieldType_Data                 = 25,
+   FIXFieldType_TZTimeOnly           = 26,
+   FIXFieldType_TZTimestamp          = 27,
+   FIXFieldType_XMLData              = 28,
+   FIXFieldType_Language             = 29
+} FIXFieldTypeEnum;
 
-int fix_message_to_string(FIXMsg* msg, char delimiter, char* buff, uint32_t buffLen);
+#define IS_STRING_TYPE(type) \
+   (type == FIXFieldType_String || type == FIXFieldType_MultipleValueString || type == FIXFieldType_Country || \
+    type == FIXFieldType_Currency || type == FIXFieldType_Exchange || type == FIXFieldType_MonthYear || \
+    type == FIXFieldType_UTCTimestamp || type == FIXFieldType_UTCTimeOnly || type == FIXFieldType_UTCTimeOnly || \
+    type == FIXFieldType_UTCDateOnly || type == FIXFieldType_LocalMktDate || type == FIXFieldType_TZTimeOnly || \
+    type == FIXFieldType_TZTimestamp)
 
-#endif /* FIX_PARSER_FIX_MSG_H */
+#define IS_INT_TYPE(type) \
+   (type == FIXFieldType_Int || type == FIXFieldType_Length || type == FIXFieldType_NumInGroup || type == FIXFieldType_SeqNum || \
+    type == FIXFieldType_TagNum || type == FIXFieldType_DayOfMonth)
+
+#define IS_FLOAT_TYPE(type) \
+   (type == FIXFieldType_Float || type == FIXFieldType_Qty || type == FIXFieldType_Price || type == FIXFieldType_PriceOffset || \
+    type == FIXFieldType_Amt || type == FIXFieldType_Percentage)
+
+#define IS_CHAR_TYPE(type) \
+   (type == FIXFieldType_Char || type == FIXFieldType_Boolean)
+
+FIXProtocolVerEnum str2FIXProtocolVerEnum(char const* ver);
+char const* FIXProtocolVerEnum2BeginString(FIXProtocolVerEnum ver);
+FIXFieldTypeEnum str2FIXFIXFieldType(char const* type);
+
+#endif /* FIX_PARSER_FIX_TYPES_H */
