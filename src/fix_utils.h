67a172ff291854ede8d87af50ced4d4e5c6dee0f
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-31 15:50:59 +0400

    Update src/fix_utils.h

--- src/fix_utils.h
+++ src/fix_utils.h
@@ -12,5 +12,9 @@
 #define UNLIKE(x)  __builtin_expect(!!(x), 0)
 
 uint32_t hash_string(char const* s);
+int numdigits(long val);
+long pow10(int n);
+int ltoa(long val, char* buff, size_t buffLen);
+int dtoa(double val, char* buff, size_t buffLen);
 
 #endif /* FIX_PARSER_FIX_UTILS_H */
