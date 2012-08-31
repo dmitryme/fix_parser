c72bed7ab0c3adb0a67bdd5a29341a089b1479b9
Dmitry Melnikov <dmitryme@gmail.com>
2012-08-31 16:03:00 +0400

    Update perf_test/perf_test.c

--- perf_test/perf_test.c
+++ perf_test/perf_test.c
@@ -9,17 +9,8 @@
 #include <stdio.h>
 #include <time.h>
 
-int main()
+void create_msg_test(FIXParser* parser)
 {
-   FIXParser* parser = fix_parser_create(512, 0, 2, 0, 1000, 0, 0/*FIXParserFlags_Validate*/);
-
-   int res = fix_protocol_init(parser, "fix44.xml");
-   if (res == FIX_FAILED)
-   {
-      printf("ERROR: %s\n", get_fix_error_text(parser));
-      return 1;
-   }
-
    struct timespec start, stop;
 
    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
@@ -30,38 +21,103 @@ int main()
       if (!msg)
       {
          printf("ERROR: %s\n", get_fix_error_text(parser));
-         return 1;
+         return;
       }
 
-      fix_msg_set_string(msg, NULL, 49, "QWERTY_12345678");
-      fix_msg_set_string(msg, NULL, 56, "ABCQWE_XYZ");
-      fix_msg_set_int(msg, NULL, 34, 34);
-      fix_msg_set_string(msg, NULL, 57, "srv-ivanov_ii1");
-      fix_msg_set_string(msg, NULL, 52, "20120716-06:00:16.230");
-      fix_msg_set_string(msg, NULL, 37, "1");
-      fix_msg_set_string(msg, NULL, 11, "CL_ORD_ID_1234567");
-      fix_msg_set_string(msg, NULL, 17, "FE_1_9494_1");
-      fix_msg_set_char(msg, NULL, 150, '0');
-      fix_msg_set_char(msg, NULL, 39, '1');
-      fix_msg_set_string(msg, NULL, 1, "ZUM");
-      fix_msg_set_string(msg, NULL, 55, "RTS-12.12");
-      fix_msg_set_char(msg, NULL, 54, '1');
-      fix_msg_set_float(msg, NULL, 38, 25);
-      fix_msg_set_float(msg, NULL, 44, 135155.0);
-      fix_msg_set_char(msg, NULL, 59, '0');
-      fix_msg_set_float(msg, NULL, 32, 0);
-      fix_msg_set_float(msg, NULL, 31, 0.0);
-      fix_msg_set_float(msg, NULL, 151, 25.0);
-      fix_msg_set_float(msg, NULL, 14, 0);
-      fix_msg_set_float(msg, NULL, 6, 0.0);
-      fix_msg_set_char(msg, NULL, 21, '1');
-      fix_msg_set_string(msg, NULL, 58, "COMMENT12");
+      fix_msg_set_string(msg, NULL, FIXTagNum_SenderCompID, "QWERTY_12345678");
+      fix_msg_set_string(msg, NULL, FIXTagNum_TargetCompID, "ABCQWE_XYZ");
+      fix_msg_set_int(msg, NULL, FIXTagNum_MsgSeqNum, 34);
+      fix_msg_set_string(msg, NULL, FIXTagNum_TargetSubID, "srv-ivanov_ii1");
+      fix_msg_set_string(msg, NULL, FIXTagNum_SendingTime, "20120716-06:00:16.230");
+      fix_msg_set_string(msg, NULL, FIXTagNum_OrderID, "1");
+      fix_msg_set_string(msg, NULL, FIXTagNum_ClOrdID, "CL_ORD_ID_1234567");
+      fix_msg_set_string(msg, NULL, FIXTagNum_ExecID, "FE_1_9494_1");
+      fix_msg_set_char(msg, NULL, FIXTagNum_ExecType, '0');
+      fix_msg_set_char(msg, NULL, FIXTagNum_OrdStatus, '1');
+      fix_msg_set_string(msg, NULL, FIXTagNum_Account, "ZUM");
+      fix_msg_set_string(msg, NULL, FIXTagNum_Symbol, "RTS-12.12");
+      fix_msg_set_char(msg, NULL, FIXTagNum_Side, '1');
+      fix_msg_set_float(msg, NULL, FIXTagNum_OrderQty, 25);
+      fix_msg_set_float(msg, NULL, FIXTagNum_Price, 135155.0);
+      fix_msg_set_char(msg, NULL, FIXTagNum_TimeInForce, '0');
+      fix_msg_set_float(msg, NULL, FIXTagNum_LastQty, 0);
+      fix_msg_set_float(msg, NULL, FIXTagNum_LastPx, 0.0);
+      fix_msg_set_float(msg, NULL, FIXTagNum_LeavesQty, 25.0);
+      fix_msg_set_float(msg, NULL, FIXTagNum_CumQty, 0);
+      fix_msg_set_float(msg, NULL, FIXTagNum_AvgPx, 0.0);
+      fix_msg_set_char(msg, NULL, FIXTagNum_HandlInst, '1');
+      fix_msg_set_string(msg, NULL, FIXTagNum_Text, "COMMENT12");
+
       fix_msg_free(msg);
    }
 
    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+   printf("CREATE_MSG %ld\n", (stop.tv_sec - start.tv_sec) * 1000000 + (stop.tv_nsec - start.tv_nsec) / 1000);
+}
 
-   printf("TM %ld\n", (stop.tv_sec - start.tv_sec) * 1000000 + (stop.tv_nsec - start.tv_nsec) / 1000);
+void msg_to_string(FIXParser* parser)
+{
+   struct timespec start, stop;
+
+   FIXMsg* msg = fix_msg_create(parser, FIX44, "8");
+   if (!msg)
+   {
+      printf("ERROR: %s\n", get_fix_error_text(parser));
+      return;
+   }
+
+   fix_msg_set_string(msg, NULL, FIXTagNum_SenderCompID, "QWERTY_12345678");
+   fix_msg_set_string(msg, NULL, FIXTagNum_TargetCompID, "ABCQWE_XYZ");
+   fix_msg_set_int(msg, NULL, FIXTagNum_MsgSeqNum, 34);
+   fix_msg_set_string(msg, NULL, FIXTagNum_TargetSubID, "srv-ivanov_ii1");
+   fix_msg_set_string(msg, NULL, FIXTagNum_SendingTime, "20120716-06:00:16.230");
+   fix_msg_set_string(msg, NULL, FIXTagNum_OrderID, "1");
+   fix_msg_set_string(msg, NULL, FIXTagNum_ClOrdID, "CL_ORD_ID_1234567");
+   fix_msg_set_string(msg, NULL, FIXTagNum_ExecID, "FE_1_9494_1");
+   fix_msg_set_char(msg, NULL, FIXTagNum_ExecType, '0');
+   fix_msg_set_char(msg, NULL, FIXTagNum_OrdStatus, '1');
+   fix_msg_set_string(msg, NULL, FIXTagNum_Account, "ZUM");
+   fix_msg_set_string(msg, NULL, FIXTagNum_Symbol, "RTS-12.12");
+   fix_msg_set_char(msg, NULL, FIXTagNum_Side, '1');
+   fix_msg_set_float(msg, NULL, FIXTagNum_OrderQty, 25);
+   fix_msg_set_float(msg, NULL, FIXTagNum_Price, 135155.0);
+   fix_msg_set_char(msg, NULL, FIXTagNum_TimeInForce, '0');
+   fix_msg_set_float(msg, NULL, FIXTagNum_LastQty, 0);
+   fix_msg_set_float(msg, NULL, FIXTagNum_LastPx, 0.0);
+   fix_msg_set_float(msg, NULL, FIXTagNum_LeavesQty, 25.0);
+   fix_msg_set_float(msg, NULL, FIXTagNum_CumQty, 0);
+   fix_msg_set_float(msg, NULL, FIXTagNum_AvgPx, 0.0);
+   fix_msg_set_char(msg, NULL, FIXTagNum_HandlInst, '1');
+   fix_msg_set_string(msg, NULL, FIXTagNum_Text, "COMMENT12");
+
+   clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
+   for(int i = 0; i < 100000; ++i)
+   {
+      char buff[1024];
+      fix_msg_to_string(msg, 1, buff, sizeof(buff));
+   }
+
+   clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+
+   fix_msg_free(msg);
+
+   printf("MSG_TO_STR %ld\n", (stop.tv_sec - start.tv_sec) * 1000000 + (stop.tv_nsec - start.tv_nsec) / 1000);
+}
+
+int main()
+{
+   FIXParser* parser = fix_parser_create(1024, 0, 2, 0, 1000, 0, 0/*FIXParserFlags_Validate*/);
+
+   int res = fix_protocol_init(parser, "fix44.xml");
+   if (res == FIX_FAILED)
+   {
+      printf("ERROR: %s\n", get_fix_error_text(parser));
+      return 1;
+   }
+
+   create_msg_test(parser);
+   msg_to_string(parser);
 
    return 0;
 }
