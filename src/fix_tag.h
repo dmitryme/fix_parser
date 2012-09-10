3db0e3c8b29bc3e56b5604a5d57c5839a995fcb9
Dmitry Melnikov <dmitryme@gmail.com>
2012-09-10 10:32:40 +0400

    Update src/fix_tag.h

--- src/fix_tag.h
+++ src/fix_tag.h
@@ -10,20 +10,22 @@
 
 #include <stdint.h>
 
-#define TABLE_SIZE 64
+#define GROUP_SIZE 64
 
 struct FIXTag_
 {
    uint32_t num;
    FIXTagTypeEnum type;
    struct FIXTag_* next;
+   uint32_t body_len;
+   uint32_t size;
    void* data;
 } __attribute__((packed));
 
 typedef struct FIXTagTable_
 {
-   FIXTag* tags[TABLE_SIZE];
-   struct FIXTagTable_* next;
+   FIXTag* tags[GROUP_SIZE];
+   struct FIXGroup_* next;
 } __attribute__((packed)) FIXGroup;
 
 typedef struct FIXGroups_
@@ -36,7 +38,7 @@ FIXTag* fix_tag_set(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, unsigned char c
 FIXTag* fix_tag_get(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
 int fix_tag_del(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
 
-FIXGroup* fix_group_add(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
+FIXGroup* fix_group_add(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, FIXTag** tag);
 FIXGroup* fix_group_get(FIXMsg* msg, FIXGroup* tbl, uint32_t tagNum, uint32_t grpIdx);
 int fix_group_del(FIXMsg* msg, FIXGroup* tbl, uint32_t tagNum, uint32_t grpIdx);
 
