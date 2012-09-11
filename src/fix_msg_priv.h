33bbb67086ee212fcd7a4b0aefe20d3c80d8d38c
Dmitry Melnikov <dmitryme@gmail.com>
2012-09-11 17:55:51 +0400

    Update src/fix_msg_priv.h

--- src/fix_msg_priv.h
+++ src/fix_msg_priv.h
@@ -29,9 +29,12 @@ void* fix_msg_realloc(FIXMsg* msg, void* ptr, uint32_t size);
 FIXTag* fix_msg_get_tag(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
 FIXTag* fix_msg_set_tag(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, unsigned char const* data, uint32_t len);
 FIXTag* fix_msg_set_tag_fmt(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, char const* fmt, ...);
-int fix_msg_del_tag(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
+int32_t fix_msg_del_tag(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
 
 FIXGroup* fix_msg_alloc_group(FIXMsg* msg);
 void fix_msg_free_group(FIXMsg* msg, FIXGroup* grp);
+
+int32_t fix_tag_to_fix_msg(FIXParser* parser, FIXTag* tag, char delimiter, char** buff, uint32_t* buffLen);
+int32_t int32_to_fix_msg(FIXParser* parser, uint32_t tagNum, int32_t val, char delimiter, char** buff, uint32_t* buffLen);
 
 #endif /* FIX_PARSER_FIX_MSG_PRIV_H */
