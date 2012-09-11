ba3b3656a3559a82c45b835f76b41195ade2813f
Dmitry Melnikov <dmitryme@gmail.com>
2012-09-11 17:59:19 +0400

    Update src/fix_tag.h

--- src/fix_tag.h
+++ src/fix_tag.h
@@ -7,10 +7,17 @@
 #define FIX_PARSER_FIX_TAG_H
 
 #include "fix_types.h"
+#include "fix_msg_priv.h"
 
 #include <stdint.h>
+#include <stdlib.h>
 
 #define GROUP_SIZE 64
+
+#define FIND_TAG_STEP \
+if (!it) return NULL; \
+if (it->num == tagNum) return it; \
+it = it->next;
 
 struct FIXTag_
 {
@@ -34,11 +41,28 @@ typedef struct FIXGroups_
 } FIXGroups;
 
 FIXTag* fix_tag_set(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, unsigned char const* data, uint32_t len);
-FIXTag* fix_tag_get(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
-int fix_tag_del(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
+
+inline static FIXTag* fix_tag_get(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum)
+{
+   uint32_t const idx = tagNum % GROUP_SIZE;
+   FIXTag* it = (grp ? grp : msg->tags)->tags[idx];
+   FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;
+   FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;
+   FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;
+   FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;
+   FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;
+   FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;
+   FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;
+   FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;
+   FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;
+   FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;FIND_TAG_STEP;
+   return NULL;
+}
+
+int32_t fix_tag_del(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
 
 FIXGroup* fix_group_add(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, FIXTag** tag);
 FIXGroup* fix_group_get(FIXMsg* msg, FIXGroup* tbl, uint32_t tagNum, uint32_t grpIdx);
-int fix_group_del(FIXMsg* msg, FIXGroup* tbl, uint32_t tagNum, uint32_t grpIdx);
+int32_t fix_group_del(FIXMsg* msg, FIXGroup* tbl, uint32_t tagNum, uint32_t grpIdx);
 
 #endif /* FIX_PARSER_FIX_TAG_H */
