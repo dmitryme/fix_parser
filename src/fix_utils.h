fd7a8fdff007ab58f12db44e79ec83e9e3ab1815
Dmitry Melnikov <dmitryme@gmail.com>
2012-09-14 14:52:07 +0400

    Update src/fix_utils.h

--- src/fix_utils.h
+++ src/fix_utils.h
@@ -16,7 +16,7 @@ int32_t fix_utils_numdigits(int64_t val);
 long fix_utils_lpow10(int n);
 int32_t fix_utils_i64toa(int64_t val, char* buff, uint32_t buffLen, char padSym);
 int32_t fix_utils_dtoa(double val, char* buff, uint32_t buffLen);
-int32_t fix_utils_atoi64(char const* buff, uint32_t buffLen, int64_t* val);
-int32_t fix_utils_atod(char const* buff, uint32_t buffLen, double* val);
+int32_t fix_utils_atoi64(char const* buff, uint32_t buffLen, char stopChar, int64_t* val);
+int32_t fix_utils_atod(char const* buff, uint32_t buffLen, char stopChar, double* val);
 
 #endif /* FIX_PARSER_FIX_UTILS_H */
