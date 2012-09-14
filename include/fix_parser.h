41118e73fbeaf97376801f2de3b8068b701a64c9
Dmitry Melnikov <dmitryme@gmail.com>
2012-09-14 17:58:31 +0400

    Update include/fix_parser.h

--- include/fix_parser.h
+++ include/fix_parser.h
@@ -21,6 +21,6 @@ char const* get_fix_error_text(FIXParser* parser);
 int32_t get_fix_parser_flags(FIXParser* parser);
 
 int32_t fix_protocol_init(FIXParser*, char const* protFile);
-int32_t parse_fix(FIXParser* parser, char const* data, uint32_t len, FIXProtocolVerEnum ver, FIXMsg** msg);
+int32_t parse_fix(FIXParser* parser, char const* data, uint32_t len, FIXProtocolVerEnum ver, char delimiter, FIXMsg** msg);
 
 #endif /* FIX_PARSER_FIX_PARSER_H */
