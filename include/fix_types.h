5d1e22048362a9f280233a4ee2fd9b33bb3b88b1
Dmitry Melnikov <dmitryme@gmail.com>
2012-09-28 18:48:34 +0400

    Update include/fix_types.h

--- include/fix_types.h
+++ include/fix_types.h
@@ -6,12 +6,12 @@
 #ifndef FIX_PARSER_FIX_TYPES_H
 #define FIX_PARSER_FIX_TYPES_H
 
-#include "fix_tag_num.h"
+#include "fix_field_tag.h"
 
 #define FIX_FAILED                        -1
 #define FIX_SUCCESS                        0
-#define FIX_ERROR_TAG_HAS_WRONG_TYPE       1
-#define FIX_ERROR_TAG_NOT_FOUND            2
+#define FIX_ERROR_FIELD_HAS_WRONG_TYPE     1
+#define FIX_ERROR_FIELD_NOT_FOUND          2
 #define FIX_ERROR_GROUP_WRONG_INDEX        3
 #define FIX_ERROR_XML_ATTR_NOT_FOUND       4
 #define FIX_ERROR_XML_ATTR_WRONG_VALUE     5
@@ -32,7 +32,7 @@
 #define FIX_ERROR_WRONG_FIELD             20
 
 typedef struct FIXGroup_ FIXGroup;
-typedef struct FIXTag_ FIXTag;
+typedef struct FIXField_ FIXField;
 typedef struct FIXMsg_ FIXMsg;
 typedef struct FIXParser_ FIXParser;
 
@@ -42,11 +42,11 @@ typedef struct FIXParser_ FIXParser;
 #define PARSER_FLAG_CHECK_VALUE    0x08
 #define PARSER_FLAG_CHECK_ALL (PARSER_FLAG_CHECK_CRC | PARSER_FLAG_CHECK_REQUIRED | PARSER_FLAG_CHECK_EXISTING | PARSER_FLAG_CHECK_VALUE)
 
-typedef enum FIXTagTypeEnum
+typedef enum FIXFieldTypeEnum
 {
-   FIXTagType_Value = 1,
-   FIXTagType_Group = 2
-} FIXTagTypeEnum;
+   FIXFieldType_Value = 1,
+   FIXFieldType_Group = 2
+} FIXFieldTypeEnum;
 
 typedef enum FIXProtocolVerEnum
 {
@@ -59,39 +59,39 @@ typedef enum FIXProtocolVerEnum
    FIX_MUST_BE_LAST_DO_NOT_USE_OR_CHANGE_IT
 } FIXProtocolVerEnum;
 
-typedef enum FIXFieldTypeEnum
+typedef enum FIXFieldValueTypeEnum
 {
-   FIXFieldType_Unknown              = -1,
-   FIXFieldType_Int                  = 0x01,
-   FIXFieldType_Length               = 0x02,
-   FIXFieldType_NumInGroup           = 0x03,
-   FIXFieldType_SeqNum               = 0x04,
-   FIXFieldType_TagNum               = 0x05,
-   FIXFieldType_DayOfMonth           = 0x06,
-   FIXFieldType_Float                = 0x10,
-   FIXFieldType_Qty                  = 0x20,
-   FIXFieldType_Price                = 0x30,
-   FIXFieldType_PriceOffset          = 0x40,
-   FIXFieldType_Amt                  = 0x50,
-   FIXFieldType_Percentage           = 0x60,
-   FIXFieldType_Char                 = 0x100,
-   FIXFieldType_Boolean              = 0x200,
-   FIXFieldType_String               = 0x1000,
-   FIXFieldType_MultipleValueString  = 0x2000,
-   FIXFieldType_Country              = 0x3000,
-   FIXFieldType_Currency             = 0x4000,
-   FIXFieldType_Exchange             = 0x5000,
-   FIXFieldType_MonthYear            = 0x6000,
-   FIXFieldType_UTCTimestamp         = 0x7000,
-   FIXFieldType_UTCTimeOnly          = 0x8000,
-   FIXFieldType_UTCDateOnly          = 0x9000,
-   FIXFieldType_LocalMktDate         = 0xA000,
-   FIXFieldType_TZTimeOnly           = 0xB000,
-   FIXFieldType_TZTimestamp          = 0xC000,
-   FIXFieldType_Language             = 0xD000,
-   FIXFieldType_XMLData              = 0x20000,
-   FIXFieldType_Data                 = 0x10000
-} FIXFieldTypeEnum;
+   FIXFieldValueType_Unknown              = -1,
+   FIXFieldValueType_Int                  = 0x01,
+   FIXFieldValueType_Length               = 0x02,
+   FIXFieldValueType_NumInGroup           = 0x03,
+   FIXFieldValueType_SeqNum               = 0x04,
+   FIXFieldValueType_TagNum               = 0x05,
+   FIXFieldValueType_DayOfMonth           = 0x06,
+   FIXFieldValueType_Float                = 0x10,
+   FIXFieldValueType_Qty                  = 0x20,
+   FIXFieldValueType_Price                = 0x30,
+   FIXFieldValueType_PriceOffset          = 0x40,
+   FIXFieldValueType_Amt                  = 0x50,
+   FIXFieldValueType_Percentage           = 0x60,
+   FIXFieldValueType_Char                 = 0x100,
+   FIXFieldValueType_Boolean              = 0x200,
+   FIXFieldValueType_String               = 0x1000,
+   FIXFieldValueType_MultipleValueString  = 0x2000,
+   FIXFieldValueType_Country              = 0x3000,
+   FIXFieldValueType_Currency             = 0x4000,
+   FIXFieldValueType_Exchange             = 0x5000,
+   FIXFieldValueType_MonthYear            = 0x6000,
+   FIXFieldValueType_UTCTimestamp         = 0x7000,
+   FIXFieldValueType_UTCTimeOnly          = 0x8000,
+   FIXFieldValueType_UTCDateOnly          = 0x9000,
+   FIXFieldValueType_LocalMktDate         = 0xA000,
+   FIXFieldValueType_TZTimeOnly           = 0xB000,
+   FIXFieldValueType_TZTimestamp          = 0xC000,
+   FIXFieldValueType_Language             = 0xD000,
+   FIXFieldValueType_XMLData              = 0x20000,
+   FIXFieldValueType_Data                 = 0x10000
+} FIXFieldValueTypeEnum;
 
 #define IS_STRING_TYPE(type) ((type & 0xF000) > 0)
 #define IS_INT_TYPE(type)    ((type & 0x0F) > 0)
@@ -101,6 +101,6 @@ typedef enum FIXFieldTypeEnum
 
 FIXProtocolVerEnum str2FIXProtocolVerEnum(char const* ver);
 char const* FIXProtocolVerEnum2BeginString(FIXProtocolVerEnum ver);
-FIXFieldTypeEnum str2FIXFIXFieldType(char const* type);
+FIXFieldValueTypeEnum str2FIXFieldValueType(char const* type);
 
 #endif /* FIX_PARSER_FIX_TYPES_H */
