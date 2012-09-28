4ac134b1e9bf07516abd4cfce2d0fe53536e2759
Dmitry Melnikov <dmitryme@gmail.com>
2012-09-28 18:51:05 +0400

    Update src/fix_msg_priv.h

--- src/fix_msg_priv.h
+++ src/fix_msg_priv.h
@@ -16,7 +16,7 @@ struct FIXMsg_
 {
    FIXParser* parser;
    FIXMsgDescr* descr;
-   FIXGroup* tags;
+   FIXGroup* fields;
    FIXPage* pages;
    FIXPage* curr_page;
    FIXGroup* used_groups;
@@ -26,16 +26,15 @@ struct FIXMsg_
 void* fix_msg_alloc(FIXMsg* msg, uint32_t size);
 void* fix_msg_realloc(FIXMsg* msg, void* ptr, uint32_t size);
 
-FIXTag* fix_msg_get_tag(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
-FIXTag* fix_msg_set_tag(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, unsigned char const* data, uint32_t len);
-FIXTag* fix_msg_set_tag_fmt(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum, char const* fmt, ...);
-int32_t fix_msg_del_tag(FIXMsg* msg, FIXGroup* grp, uint32_t tagNum);
+FIXField* fix_msg_get_field(FIXMsg* msg, FIXGroup* grp, uint32_t tag);
+FIXField* fix_msg_set_field(FIXMsg* msg, FIXGroup* grp, uint32_t tag, unsigned char const* data, uint32_t len);
+int32_t fix_msg_del_field(FIXMsg* msg, FIXGroup* grp, uint32_t tag);
 
 FIXGroup* fix_msg_alloc_group(FIXMsg* msg);
 void fix_msg_free_group(FIXMsg* msg, FIXGroup* grp);
 
-int32_t fix_tag_to_fix_msg(FIXParser* parser, FIXTag* tag, char delimiter, char** buff, uint32_t* buffLen);
+int32_t fix_field_to_fix_msg(FIXParser* parser, FIXField* field, char delimiter, char** buff, uint32_t* buffLen);
 int32_t int32_to_fix_msg(FIXParser* parser, uint32_t tagNum, int32_t val, char delimiter, uint32_t width, char padSym, char** buff, uint32_t* buffLen);
-int32_t fix_groups_to_string(FIXMsg* msg, FIXTag* tag, FIXFieldDescr* fdescr, char delimiter, char** buff, uint32_t* buffLen);
+int32_t fix_groups_to_string(FIXMsg* msg, FIXField* field, FIXFieldDescr* fdescr, char delimiter, char** buff, uint32_t* buffLen);
 
 #endif /* FIX_PARSER_FIX_MSG_PRIV_H */
