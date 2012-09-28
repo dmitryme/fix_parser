c20a060d6ff57fa7fa53bbd2648662ee58a36a48
Dmitry Melnikov <dmitryme@gmail.com>
2012-09-28 18:53:03 +0400

    Update src/fix_types.c

--- src/fix_types.c
+++ src/fix_types.c
@@ -32,36 +32,36 @@ char const* FIXProtocolVerEnum2BeginString(FIXProtocolVerEnum ver)
    }
 }
 
-FIXFieldTypeEnum str2FIXFIXFieldType(char const* type)
+FIXFieldValueTypeEnum str2FIXFieldValueType(char const* type)
 {
-   if (!strcmp(type, "Int"))           { return FIXFieldType_Int; }
-   if (!strcmp(type, "Length"))        { return FIXFieldType_Length; }
-   if (!strcmp(type, "NumInGroup"))    { return FIXFieldType_NumInGroup; }
-   if (!strcmp(type, "SeqNum"))        { return FIXFieldType_SeqNum; }
-   if (!strcmp(type, "TagNum"))        { return FIXFieldType_TagNum; }
-   if (!strcmp(type, "DayOfMonth"))    { return FIXFieldType_DayOfMonth; }
-   if (!strcmp(type, "Float"))         { return FIXFieldType_Float; }
-   if (!strcmp(type, "Qty"))           { return FIXFieldType_Qty; }
-   if (!strcmp(type, "Price"))         { return FIXFieldType_Price; }
-   if (!strcmp(type, "PriceOffset"))   { return FIXFieldType_PriceOffset; }
-   if (!strcmp(type, "Amt"))           { return FIXFieldType_Amt; }
-   if (!strcmp(type, "Percentage"))    { return FIXFieldType_Percentage; }
-   if (!strcmp(type, "Char"))          { return FIXFieldType_Char; }
-   if (!strcmp(type, "Boolean"))       { return FIXFieldType_Boolean; }
-   if (!strcmp(type, "String"))        { return FIXFieldType_String; }
-   if (!strcmp(type, "MultipleValueString")) { return FIXFieldType_MultipleValueString; }
-   if (!strcmp(type, "Country"))       { return FIXFieldType_Country; }
-   if (!strcmp(type, "Currency"))      { return FIXFieldType_Currency; }
-   if (!strcmp(type, "Exchange"))      { return FIXFieldType_Exchange; }
-   if (!strcmp(type, "MonthYear"))     { return FIXFieldType_MonthYear; }
-   if (!strcmp(type, "UTCTimestamp"))  { return FIXFieldType_UTCTimestamp; }
-   if (!strcmp(type, "UTCTimeOnly"))   { return FIXFieldType_UTCTimeOnly; }
-   if (!strcmp(type, "UTCDateOnly"))   { return FIXFieldType_UTCDateOnly; }
-   if (!strcmp(type, "LocalMktDate"))  { return FIXFieldType_LocalMktDate; }
-   if (!strcmp(type, "Data"))          { return FIXFieldType_Data; }
-   if (!strcmp(type, "TZTimeOnly"))    { return FIXFieldType_TZTimeOnly; }
-   if (!strcmp(type, "TZTimestamp"))   { return FIXFieldType_TZTimestamp; }
-   if (!strcmp(type, "XMLData"))       { return FIXFieldType_XMLData; }
-   if (!strcmp(type, "Language"))      { return FIXFieldType_Language; }
-   return FIXFieldType_Unknown;
+   if (!strcmp(type, "Int"))           { return FIXFieldValueType_Int; }
+   if (!strcmp(type, "Length"))        { return FIXFieldValueType_Length; }
+   if (!strcmp(type, "NumInGroup"))    { return FIXFieldValueType_NumInGroup; }
+   if (!strcmp(type, "SeqNum"))        { return FIXFieldValueType_SeqNum; }
+   if (!strcmp(type, "TagNum"))        { return FIXFieldValueType_TagNum; }
+   if (!strcmp(type, "DayOfMonth"))    { return FIXFieldValueType_DayOfMonth; }
+   if (!strcmp(type, "Float"))         { return FIXFieldValueType_Float; }
+   if (!strcmp(type, "Qty"))           { return FIXFieldValueType_Qty; }
+   if (!strcmp(type, "Price"))         { return FIXFieldValueType_Price; }
+   if (!strcmp(type, "PriceOffset"))   { return FIXFieldValueType_PriceOffset; }
+   if (!strcmp(type, "Amt"))           { return FIXFieldValueType_Amt; }
+   if (!strcmp(type, "Percentage"))    { return FIXFieldValueType_Percentage; }
+   if (!strcmp(type, "Char"))          { return FIXFieldValueType_Char; }
+   if (!strcmp(type, "Boolean"))       { return FIXFieldValueType_Boolean; }
+   if (!strcmp(type, "String"))        { return FIXFieldValueType_String; }
+   if (!strcmp(type, "MultipleValueString")) { return FIXFieldValueType_MultipleValueString; }
+   if (!strcmp(type, "Country"))       { return FIXFieldValueType_Country; }
+   if (!strcmp(type, "Currency"))      { return FIXFieldValueType_Currency; }
+   if (!strcmp(type, "Exchange"))      { return FIXFieldValueType_Exchange; }
+   if (!strcmp(type, "MonthYear"))     { return FIXFieldValueType_MonthYear; }
+   if (!strcmp(type, "UTCTimestamp"))  { return FIXFieldValueType_UTCTimestamp; }
+   if (!strcmp(type, "UTCTimeOnly"))   { return FIXFieldValueType_UTCTimeOnly; }
+   if (!strcmp(type, "UTCDateOnly"))   { return FIXFieldValueType_UTCDateOnly; }
+   if (!strcmp(type, "LocalMktDate"))  { return FIXFieldValueType_LocalMktDate; }
+   if (!strcmp(type, "Data"))          { return FIXFieldValueType_Data; }
+   if (!strcmp(type, "TZTimeOnly"))    { return FIXFieldValueType_TZTimeOnly; }
+   if (!strcmp(type, "TZTimestamp"))   { return FIXFieldValueType_TZTimestamp; }
+   if (!strcmp(type, "XMLData"))       { return FIXFieldValueType_XMLData; }
+   if (!strcmp(type, "Language"))      { return FIXFieldValueType_Language; }
+   return FIXFieldValueType_Unknown;
 }
