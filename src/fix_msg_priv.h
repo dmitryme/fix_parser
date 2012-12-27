ee3e9452b22462b1b27e690e70d6da7defeb0cee
Dmitry Melnikov <dmitryme@gmail.com>
2012-12-27 13:09:39 +0400

    Update src/fix_msg_priv.h

--- src/fix_msg_priv.h
+++ src/fix_msg_priv.h
@@ -33,16 +33,16 @@ struct FIXMsg_
 void* fix_msg_alloc(FIXMsg* msg, uint32_t size);
 void* fix_msg_realloc(FIXMsg* msg, void* ptr, uint32_t size);
 
-FIXField* fix_msg_get_field(FIXMsg* msg, FIXGroup* grp, uint32_t tag);
+FIXField* fix_msg_get_field(FIXMsg* msg, FIXGroup* grp, FIXTagNum tag);
 FIXField* fix_msg_set_field(FIXMsg* msg, FIXGroup* grp, FIXFieldDescr* fdescr, unsigned char const* data, uint32_t len);
-int32_t fix_msg_del_field(FIXMsg* msg, FIXGroup* grp, uint32_t tag);
+int32_t fix_msg_del_field(FIXMsg* msg, FIXGroup* grp, FIXTagNum tag);
 
 FIXGroup* fix_msg_alloc_group(FIXMsg* msg);
 void fix_msg_free_group(FIXMsg* msg, FIXGroup* grp);
 
-int32_t fix_groups_to_string(FIXMsg* msg, FIXField* field, FIXFieldDescr* fdescr, char delimiter, char** buff, uint32_t* buffLen);
-int32_t int32_to_fix_msg(FIXParser* parser, uint32_t tag, int32_t val, char delimiter, uint32_t width, char padSym, char** buff, uint32_t* buffLen);
-int32_t fix_field_to_fix_msg(FIXParser* parser, FIXField* field, char delimiter, char** buff, uint32_t* buffLen);
+FIXErrCode fix_groups_to_string(FIXMsg* msg, FIXField* field, FIXFieldDescr* fdescr, char delimiter, char** buff, uint32_t* buffLen);
+FIXErrCode int32_to_fix_msg(FIXParser* parser, FIXTagNum tag, int32_t val, char delimiter, uint32_t width, char padSym, char** buff, uint32_t* buffLen);
+FIXErrCode fix_field_to_fix_msg(FIXParser* parser, FIXField* field, char delimiter, char** buff, uint32_t* buffLen);
 
 #ifdef __cplusplus
 }
