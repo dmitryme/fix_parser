682f1331f2b6e1a880ed3f9c9df769f3fca6d97c
Dmitry Melnikov <dmitryme@gmail.com>
2013-01-08 09:22:12 +0400

    Update build_vs10_test.bat

--- build_vs10_test.bat
+++ build_vs10_test.bat
@@ -2,6 +2,6 @@ call "%VS100COMNTOOLS%\vsvars32.bat"
 
 mkdir build
 pushd build
-cmake -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=YES -G "Visual Studio 10" ../
+cmake -DWITH_TESTS=YES -G "Visual Studio 10" ../
 
 popd
