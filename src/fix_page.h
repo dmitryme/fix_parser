9fd5c2aa59406286d1f786e6497172bc4cdb3127
Dmitry Melnikov <dmitryme@gmail.com>
2013-01-10 17:16:05 +0400

    Update src/fix_page.h

--- src/fix_page.h
+++ src/fix_page.h
@@ -1,7 +1,8 @@
-/* @file   fix_page.h
-   @author Dmitry S. Melnikov, dmitryme@gmail.com
-   @date   Created on: 08/02/2012 04:34:14 PM
-*/
+/**
+ * @file   fix_page.h
+ * @author Dmitry S. Melnikov, dmitryme@gmail.com
+ * @date   Created on: 08/02/2012 04:34:14 PM
+ */
 
 #ifndef FIX_PARSER_FIX_PAGE_H
 #define FIX_PARSER_FIX_PAGE_H
@@ -11,12 +12,15 @@
 #pragma pack(push, 1)
 #pragma pack(1)
 
+/**
+ * page of memory for storing FIX fields
+ */
 typedef struct FIXPage_
 {
-   uint32_t size;
-   uint32_t offset;
-   struct FIXPage_* next;
-   char data[1];
+   uint32_t size;          ///< size of page
+   uint32_t offset;        ///< offset of free space
+   struct FIXPage_* next;  ///< next page
+   char data[1];           ///< start of data
 } FIXPage;
 
 #pragma pack(pop)
