0c2e5070ad35f108ba6fa22773c2648253e3c76e
Dmitry Melnikov <dmitryme@gmail.com>
2013-01-10 17:12:11 +0400

    Update src/fix_error.c

--- src/fix_error.c
+++ src/fix_error.c
@@ -1,7 +1,8 @@
-/* @file   fix_error.c
-   @author Dmitry S. Melnikov, dmitryme@gmail.com
-   @date   Created on: 07/30/2012 10:26:54 AM
-*/
+/**
+ * @file   fix_error.c
+ * @author Dmitry S. Melnikov, dmitryme@gmail.com
+ * @date   Created on: 07/30/2012 10:26:54 AM
+ */
 
 #include "fix_utils.h"
 #include "fix_error.h"
