529f35586ebe3eca2cb97f1721785f889d4e0f1c
Dmitry Melnikov <dmitryme@gmail.com>
2013-01-15 17:14:19 +0400

    Update test/fix_msg_tests.cc

--- test/fix_msg_tests.cc
+++ test/fix_msg_tests.cc
@@ -143,7 +143,7 @@ TEST(FixMsgTests, CreateMsg2Test)
 
    char buff[1024];
    uint32_t reqBuffLen = 0;
-   ASSERT_EQ(FIX_FAILED, fix_msg_to_fix(msg, '|', buff, sizeof(buff), &reqBuffLen));
+   ASSERT_EQ(FIX_FAILED, fix_msg_to_str(msg, '|', buff, sizeof(buff), &reqBuffLen));
 
    ASSERT_EQ(fix_msg_set_string(msg, NULL, FIXFieldTag_SenderCompID, "QWERTY_12345678"), FIX_SUCCESS);
    ASSERT_EQ(msg->body_len, 5U + 19);
@@ -222,7 +222,7 @@ TEST(FixMsgTests, ToStringTest)
 
    char buff[1024];
    uint32_t reqBuffLen = 0;
-   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_fix(msg, FIX_SOH, buff,sizeof(buff), &reqBuffLen));
+   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_str(msg, FIX_SOH, buff,sizeof(buff), &reqBuffLen));
    ASSERT_EQ(reqBuffLen, 251U);
    buff[reqBuffLen] = 0;
    ASSERT_STREQ(buff, "8=FIX.4.4\0019=228\00135=8\00149=QWERTY_12345678\00156=ABCQWE_XYZ\00134=34\00157=srv-ivanov_ii1\00152=20120716-06:00:16.230\001"
@@ -230,7 +230,7 @@ TEST(FixMsgTests, ToStringTest)
             "151=25\00114=0\0016=0\00121=1\00158=COMMENT12\00110=240\001");
 
    reqBuffLen = 0;
-   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_fix(msg, '|', buff,sizeof(buff), &reqBuffLen));
+   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_str(msg, '|', buff,sizeof(buff), &reqBuffLen));
    ASSERT_EQ(reqBuffLen, 251U);
    buff[reqBuffLen] = 0;
    ASSERT_STREQ(buff, "8=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|"
@@ -253,7 +253,7 @@ TEST(FixMsgTests, ToStringGroupTest)
 
    char buff[1024];
    uint32_t reqBuffLen = 0;
-   ASSERT_EQ(FIX_FAILED, fix_msg_to_fix(msg, '|', buff, sizeof(buff), &reqBuffLen));
+   ASSERT_EQ(FIX_FAILED, fix_msg_to_str(msg, '|', buff, sizeof(buff), &reqBuffLen));
 
    ASSERT_EQ(fix_msg_set_string(msg, NULL, FIXFieldTag_SenderCompID, "QWERTY_12345678"), FIX_SUCCESS);
    ASSERT_EQ(fix_msg_set_string(msg, NULL, FIXFieldTag_TargetCompID, "ABCQWE_XYZ"), FIX_SUCCESS);
@@ -276,14 +276,14 @@ TEST(FixMsgTests, ToStringGroupTest)
    ASSERT_EQ(fix_msg_set_char(msg, grp2, FIXFieldTag_PartyIDSource, 'B'), FIX_SUCCESS);
    ASSERT_EQ(fix_msg_set_int32(msg, grp2, FIXFieldTag_PartyRole, 2), FIX_SUCCESS);
 
-   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_fix(msg, 1, buff, sizeof(buff), &reqBuffLen));
+   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_str(msg, 1, buff, sizeof(buff), &reqBuffLen));
    buff[reqBuffLen] = 0;
    ASSERT_EQ(reqBuffLen, 213U);
    ASSERT_STREQ(buff, "8=FIX.4.4\0019=190\00135=D\00149=QWERTY_12345678\00156=ABCQWE_XYZ\00134=34\00152=20120716-06:00:16.230\001"
             "11=CL_ORD_ID_1234567\001453=2\001448=ID1\001447=A\001452=1\001448=ID2\001447=B\001452=2\00155=RTS-12.12\001"
             "54=1\00160=20120716-06:00:16.230\00138=25\00140=2\00110=088\001");
 
-   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_fix(msg, '|', buff, sizeof(buff), &reqBuffLen));
+   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_str(msg, '|', buff, sizeof(buff), &reqBuffLen));
    buff[reqBuffLen] = 0;
    ASSERT_EQ(reqBuffLen, 213U);
    ASSERT_STREQ(buff, "8=FIX.4.4|9=190|35=D|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|52=20120716-06:00:16.230|11=CL_ORD_ID_1234567|453=2|448=ID1|447=A|452=1|448=ID2|447=B|452=2|55=RTS-12.12|"
@@ -291,26 +291,26 @@ TEST(FixMsgTests, ToStringGroupTest)
 
 
    ASSERT_EQ(fix_msg_del_group(msg, NULL, FIXFieldTag_NoPartyIDs, 0), FIX_SUCCESS);
-   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_fix(msg, 1, buff, sizeof(buff), &reqBuffLen));
+   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_str(msg, 1, buff, sizeof(buff), &reqBuffLen));
    buff[reqBuffLen] = 0;
    ASSERT_EQ(reqBuffLen, 193U);
    ASSERT_STREQ(buff, "8=FIX.4.4\0019=170\00135=D\00149=QWERTY_12345678\00156=ABCQWE_XYZ\00134=34\00152=20120716-06:00:16.230\001"
             "11=CL_ORD_ID_1234567\001453=1\001448=ID2\001447=B\001452=2\00155=RTS-12.12\00154=1\00160=20120716-06:00:16.230\00138=25\00140=2\00110=145\001");
 
-   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_fix(msg, '|', buff, sizeof(buff), &reqBuffLen));
+   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_str(msg, '|', buff, sizeof(buff), &reqBuffLen));
    buff[reqBuffLen] = 0;
    ASSERT_EQ(reqBuffLen, 193U);
    ASSERT_STREQ(buff, "8=FIX.4.4|9=170|35=D|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|52=20120716-06:00:16.230|"
             "11=CL_ORD_ID_1234567|453=1|448=ID2|447=B|452=2|55=RTS-12.12|54=1|60=20120716-06:00:16.230|38=25|40=2|10=145|");
 
    ASSERT_EQ(fix_msg_del_group(msg, NULL, FIXFieldTag_NoPartyIDs, 0), FIX_SUCCESS);
-   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_fix(msg, 1, buff, sizeof(buff), &reqBuffLen));
+   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_str(msg, 1, buff, sizeof(buff), &reqBuffLen));
    buff[reqBuffLen] = 0;
    ASSERT_EQ(reqBuffLen, 167U);
    ASSERT_STREQ(buff, "8=FIX.4.4\0019=144\00135=D\00149=QWERTY_12345678\00156=ABCQWE_XYZ\00134=34\00152=20120716-06:00:16.230\001"
             "11=CL_ORD_ID_1234567\00155=RTS-12.12\00154=1\00160=20120716-06:00:16.230\00138=25\00140=2\00110=192\001");
 
-   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_fix(msg, '|', buff, sizeof(buff), &reqBuffLen));
+   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_str(msg, '|', buff, sizeof(buff), &reqBuffLen));
    buff[reqBuffLen] = 0;
    ASSERT_EQ(reqBuffLen, 167U);
    ASSERT_STREQ(buff, "8=FIX.4.4|9=144|35=D|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|52=20120716-06:00:16.230|"
