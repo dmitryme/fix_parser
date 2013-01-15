fb3875991d61327151f41f2dcffe52685b8a5c1b
Dmitry Melnikov <dmitryme@gmail.com>
2013-01-15 17:14:42 +0400

    Update test/fix_parser_tests.cc

--- test/fix_parser_tests.cc
+++ test/fix_parser_tests.cc
@@ -39,28 +39,28 @@ TEST(FixParserTests, ParseBeginStringTest)
    {
       char buff[] = "A12=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|10=240|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_PARSE_MSG);
    }
    {
       char buff[] = "1=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|10=240|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_WRONG_FIELD);
    }
    {
       char buff[] = "8=FIX.4.5|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|10=240|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_WRONG_PROTOCOL_VER);
    }
    {
       char buff[] = "8=FIXT.1.1|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|10=240|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_WRONG_PROTOCOL_VER);
    }
@@ -75,14 +75,14 @@ TEST(FixParserTests, ParseBodyLengthTest)
    {
       char buff[] = "8=FIX.4.4|10=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|10=240|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_WRONG_FIELD);
    }
    {
       char buff[] = "8=FIX.4.4|9=228A|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|10=240|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_PARSE_MSG);
    }
@@ -90,7 +90,7 @@ TEST(FixParserTests, ParseBodyLengthTest)
    {
       char buff[] = "8=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, 0);
    }
@@ -98,7 +98,7 @@ TEST(FixParserTests, ParseBodyLengthTest)
    {
       char buff[] = "8=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, 0);
    }
@@ -106,7 +106,7 @@ TEST(FixParserTests, ParseBodyLengthTest)
    {
       char buff[] = "8=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, 0);
    }
@@ -114,7 +114,7 @@ TEST(FixParserTests, ParseBodyLengthTest)
    {
       char buff[] = "8=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|10=240";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_PARSE_MSG);
    }
@@ -129,7 +129,7 @@ TEST(FixParserTests, ParseCheckSumTest)
    {
       char buff[] = "8=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|A10=240|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_PARSE_MSG);
    }
@@ -137,7 +137,7 @@ TEST(FixParserTests, ParseCheckSumTest)
    {
       char buff[] = "8=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|11=240|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_WRONG_FIELD);
    }
@@ -145,7 +145,7 @@ TEST(FixParserTests, ParseCheckSumTest)
    {
       char buff[] = "8=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|10=A240|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_PARSE_MSG);
    }
@@ -153,7 +153,7 @@ TEST(FixParserTests, ParseCheckSumTest)
    {
       char buff[] = "8=FIX.4.4|9=228|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|52=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|10=210|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_INTEGRITY_CHECK);
    }
@@ -161,7 +161,7 @@ TEST(FixParserTests, ParseCheckSumTest)
    {
       char buff[] = "8=FIX.4.4|9=230|35=8|49=QWERTY_12345678|56=ABCQWE_XYZ|34=34|57=srv-ivanov_ii1|4552=20120716-06:00:16.230|37=1|11=CL_ORD_ID_1234567|17=FE_1_9494_1|150=0|39=1|1=ZUM|55=RTS-12.12|54=1|38=25|44=135155|59=0|32=0|31=0|151=25|14=0|6=0|21=1|58=COMMENT12|10=208|";
       char const* stop = NULL;
-      FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), '|', &stop);
+      FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), '|', &stop);
       ASSERT_TRUE(msg == NULL);
       ASSERT_EQ(parser->error.code, FIX_ERROR_UNKNOWN_FIELD);
       ASSERT_TRUE(!strcmp(parser->error.text, "Field '4552' not found in description."));
@@ -203,41 +203,54 @@ TEST(FixParserTests, ParseStringTest)
       "11=CL_ORD_ID_1234567\00117=FE_1_9494_1\001150=0\00139=1\0011=ZUM\00155=RTS-12.12\00154=1\00138=25\00144=135155\00159=0\00132=0\00131=0\001151=25\001"
       "14=0\0016=0\00121=1\00158=COMMENT12\00110=240\001";
    char const* stop = NULL;
-   FIXMsg* msg = fix_parser_fix_to_msg(parser, buff, strlen(buff), FIX_SOH, &stop);
+   FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), FIX_SOH, &stop);
    ASSERT_TRUE(msg != NULL);
 
-   CHECK_STRING(msg, FIXFieldTag_BeginString, "FIX.4.4");
-   CHECK_INT32(msg, FIXFieldTag_BodyLength, 228);
-   CHECK_STRING(msg, FIXFieldTag_MsgType, "8");
+   CHECK_STRING(msg, FIXFieldTag_BeginString,  "FIX.4.4");
+   CHECK_INT32(msg,  FIXFieldTag_BodyLength,   228);
+   CHECK_STRING(msg, FIXFieldTag_MsgType,      "8");
    CHECK_STRING(msg, FIXFieldTag_SenderCompID, "QWERTY_12345678");
    CHECK_STRING(msg, FIXFieldTag_TargetCompID, "ABCQWE_XYZ");
-   CHECK_INT32(msg, FIXFieldTag_MsgSeqNum, 34);
-   CHECK_STRING(msg, FIXFieldTag_TargetSubID, "srv-ivanov_ii1");
-   CHECK_STRING(msg, FIXFieldTag_SendingTime, "20120716-06:00:16.230");
-   CHECK_INT32(msg, FIXFieldTag_OrderID, 1);
-   CHECK_STRING(msg, FIXFieldTag_ClOrdID, "CL_ORD_ID_1234567");
-   CHECK_STRING(msg, FIXFieldTag_ExecID, "FE_1_9494_1");
-   CHECK_STRING(msg, FIXFieldTag_ExecID, "FE_1_9494_1");
-   CHECK_CHAR(msg, FIXFieldTag_ExecType, '0');
-   CHECK_CHAR(msg, FIXFieldTag_OrdStatus, '1');
-   CHECK_STRING(msg, FIXFieldTag_Account, "ZUM");
-   CHECK_STRING(msg, FIXFieldTag_Symbol, "RTS-12.12");
-   CHECK_CHAR(msg, FIXFieldTag_Side, '1');
-   CHECK_DOUBLE(msg, FIXFieldTag_OrderQty, 25);
-   CHECK_DOUBLE(msg, FIXFieldTag_Price, 135155);
-   CHECK_CHAR(msg, FIXFieldTag_TimeInForce, '0');
-   CHECK_DOUBLE(msg, FIXFieldTag_LastQty, 0);
-   CHECK_DOUBLE(msg, FIXFieldTag_LastPx, 0);
-   CHECK_DOUBLE(msg, FIXFieldTag_LeavesQty, 25);
-   CHECK_DOUBLE(msg, FIXFieldTag_CumQty, 0);
-   CHECK_DOUBLE(msg, FIXFieldTag_AvgPx, 0);
-   CHECK_STRING(msg, FIXFieldTag_HandlInst, "1");
-   CHECK_STRING(msg, FIXFieldTag_Text, "COMMENT12");
-   CHECK_STRING(msg, FIXFieldTag_CheckSum, "240");
+   CHECK_INT32(msg,  FIXFieldTag_MsgSeqNum,    34);
+   CHECK_STRING(msg, FIXFieldTag_TargetSubID,  "srv-ivanov_ii1");
+   CHECK_STRING(msg, FIXFieldTag_SendingTime,  "20120716-06:00:16.230");
+   CHECK_INT32(msg,  FIXFieldTag_OrderID,      1);
+   CHECK_STRING(msg, FIXFieldTag_ClOrdID,      "CL_ORD_ID_1234567");
+   CHECK_STRING(msg, FIXFieldTag_ExecID,       "FE_1_9494_1");
+   CHECK_STRING(msg, FIXFieldTag_ExecID,       "FE_1_9494_1");
+   CHECK_CHAR(msg,   FIXFieldTag_ExecType,     '0');
+   CHECK_CHAR(msg,   FIXFieldTag_OrdStatus,    '1');
+   CHECK_STRING(msg, FIXFieldTag_Account,      "ZUM");
+   CHECK_STRING(msg, FIXFieldTag_Symbol,       "RTS-12.12");
+   CHECK_CHAR(msg,   FIXFieldTag_Side,         '1');
+   CHECK_DOUBLE(msg, FIXFieldTag_OrderQty,     25);
+   CHECK_DOUBLE(msg, FIXFieldTag_Price,        135155);
+   CHECK_CHAR(msg,   FIXFieldTag_TimeInForce,  '0');
+   CHECK_DOUBLE(msg, FIXFieldTag_LastQty,      0);
+   CHECK_DOUBLE(msg, FIXFieldTag_LastPx,       0);
+   CHECK_DOUBLE(msg, FIXFieldTag_LeavesQty,    25);
+   CHECK_DOUBLE(msg, FIXFieldTag_CumQty,       0);
+   CHECK_DOUBLE(msg, FIXFieldTag_AvgPx,        0);
+   CHECK_STRING(msg, FIXFieldTag_HandlInst,    "1");
+   CHECK_STRING(msg, FIXFieldTag_Text,         "COMMENT12");
+   CHECK_STRING(msg, FIXFieldTag_CheckSum,     "240");
 
    char buff1[1024];
    uint32_t reqBuffLen = 0;
-   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_fix(msg, FIX_SOH, buff1, sizeof(buff1), &reqBuffLen));
+   ASSERT_EQ(FIX_SUCCESS, fix_msg_to_str(msg, FIX_SOH, buff1, sizeof(buff1), &reqBuffLen));
    buff1[reqBuffLen] = 0;
    ASSERT_STREQ(buff, buff1); // Bingo!
+}
+
+TEST(FixParserTests, ParseStringGroupTest)
+{
+   FIXParser* parser = fix_parser_create("fix_descr/fix.4.4.xml", NULL, PARSER_FLAG_CHECK_ALL);
+   ASSERT_TRUE(parser != NULL);
+   ASSERT_EQ(parser->error.code, 0);
+   char buff[] = "8=FIX.4.4\0019=190\00135=D\00149=QWERTY_12345678\00156=ABCQWE_XYZ\00134=34\00152=20120716-06:00:16.230\001"
+            "11=CL_ORD_ID_1234567\001453=2\001448=ID1\001447=A\001452=1\001448=ID2\001447=B\001452=2\00155=RTS-12.12\001"
+            "54=1\00160=20120716-06:00:16.230\00138=25\00140=2\00110=088\001";
+   char const* stop = NULL;
+   FIXMsg* msg = fix_parser_str_to_msg(parser, buff, strlen(buff), FIX_SOH, &stop);
+   ASSERT_TRUE(msg != NULL);
 }
