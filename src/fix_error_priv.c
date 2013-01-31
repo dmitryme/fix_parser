8459336f47f13172e8fbbb1dcca6bfaf44d7ffb6
Dmitry Melnikov <dmitryme@gmail.com>
2013-01-31 11:36:21 +0400

    Update src/fix_error_priv.c

--- src/fix_error_priv.c
+++ src/fix_error_priv.c
@@ -9,8 +9,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-static FIXError error;
-
 /*------------------------------------------------------------------------------------------------------------------------*/
 void fix_error_set_va(FIXError* error, FIXErrCode code, char const* text, va_list ap)
 {
@@ -33,31 +31,4 @@ void fix_error_reset(FIXError* error)
 {
    error->code = 0;
    error->text[0] = 0;
-}
-
-/*------------------------------------------------------------------------------------------------------------------------*/
-FIXError* fix_error_static_get()
-{
-   return &error;
-}
-
-/*------------------------------------------------------------------------------------------------------------------------*/
-void fix_error_static_set_va(FIXErrCode code, char const* text, va_list ap)
-{
-   fix_error_set_va(&error, code, text, ap);
-}
-
-/*------------------------------------------------------------------------------------------------------------------------*/
-void fix_error_static_set(FIXErrCode code, char const* text, ...)
-{
-   va_list ap;
-   va_start(ap, text);
-   fix_error_static_set_va(code, text, ap);
-   va_end(ap);
-}
-
-/*------------------------------------------------------------------------------------------------------------------------*/
-void fix_error_static_reset()
-{
-   fix_error_reset(&error);
 }
