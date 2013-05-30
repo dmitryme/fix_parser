1257f8267bd8a44e6619fc4752a403685a84bdd4
Dmitry Melnikov <dmitryme@gmail.com>
2013-05-30 16:50:02 +0400

    Create fix_parser_app.erl

---
+++ bind/erlang/fix_parser/src/fix_parser_app.erl
@@ -0,0 +1,16 @@
+-module(fix_parser_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    fix_parser_sup:start_link().
+
+stop(_State) ->
+    ok.
